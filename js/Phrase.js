ef648cf371e6aee3bd133745ef05d1553f822a96
YKASSABI <youssef@devmyk.com>
2019-03-06 13:41:44 +0100

    step 2

---
+++ js/Phrase.js
@@ -0,0 +1,8 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+
+// eslint-disable-next-line no-unused-vars
+class Phrase {
+
+}
---
+++ js/app.js
@@ -0,0 +1,21 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * app.js */
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////testing 
+// eslint-disable-next-line no-undef
+const phrase = new Phrase();
+// eslint-disable-next-line no-undef
+const game = new Game();
+
+console.log(phrase);
+console.log(game);
